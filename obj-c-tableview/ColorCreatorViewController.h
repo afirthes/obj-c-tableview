74bada81538a61a0e2f6289e967b8746b83884af
afirthes <afirthes@gmail.com>
2021-07-10 15:49:25 +0300

    Added color creator in storyboard

---
+++ obj-c-tableview/ColorCreatorViewController.h
@@ -0,0 +1,16 @@
+//
+//  ColorCreatorViewController.h
+//  obj-c-tableview
+//
+//  Created by sehio on 10.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ColorCreatorViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
